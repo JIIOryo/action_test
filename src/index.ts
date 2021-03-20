f2a3bdd29b33dd6b6e7b070851f6bc7069ccecc7
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2021-03-20 15:59:48 +0900

    test

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import { ExecOptions } from '@actions/exec'
+
+const execute = async (command: string): Promise<string> => {
+    let output = ''
+    const options: ExecOptions = {}
+    options.listeners = {
+        stdout: (data: Buffer) => {
+            output += data.toString()
+        },
+        stderr: (data: Buffer) => {
+            console.error(data)
+        }
+    }
+    await exec.exec(command, null, options)
+    return output
+}
+
+const main = async () => {
+    try {
+        const newTag = core.getInput('ref')
+        const preTag = await execute('/bin/bash -c "git tag --sort=-creatordate | sed -n 2p"')
+        const summary = await execute(`git log --oneline --pretty=tformat:"%h %s" ${preTag.trim()}..${newTag}`)
+        core.setOutput("summary", summary)
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+main()
